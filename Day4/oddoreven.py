6130e3744037a6003f140ef03c57178bde900832
AbbyNyakara <abbynyakara@gmail.com>
2023-09-08 13:53:44 -0400

    day 4

---
+++ Day4/oddoreven.py
@@ -0,0 +1,13 @@
+""""
+    Example code 
+"""
+# get an input from the user.
+# Check whether the number is even or odd.
+
+num_input = int(input("What is the number? "))
+
+if num_input % 2 == 1:
+    print("This is an odd number")
+else:
+    print("This is an even number")
+    
