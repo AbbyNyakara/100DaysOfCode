8d954d7e4db63724f669f6051ba4bf6db2dd5e6c
AbbyNyakara <abbynyakara@gmail.com>
2023-09-12 21:21:54 -0400

    add treasure

---
+++ Day5/treasuremap.py
@@ -0,0 +1,13 @@
+"""
+ create a virtual chess game. 
+"""
+
+row_1 = ["◼️", "◼️", "◼️"]
+row_2 = ["◼️", "◼️", "◼️"]
+row_3 = ["◼️", "◼️", "◼️"]
+
+list_3 = [row_1, row_2, row_3]
+
+print(f"{row_1} \n{row_2} \n{row_3}")
+
+position = input("Where do you want to put the treasure?")
