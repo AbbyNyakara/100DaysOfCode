8ac2c4af726049b1864010d12ad99984c7a79f1c
Abigael Nyakara <mogusuab@msu.edu>
2023-10-06 09:39:47 -0400

    updates

--- Day6/highscore.py
+++ Day6/highscore.py
@@ -7,7 +7,7 @@ num_list = [23,56,43,90, 344.55, 100]
 highest_num = 0 
 
 for num in num_list:
-    if highest_num > num:
+    if num > highest_num:
         highest_num = num
 
 print(f"The biggest number is {highest_num}")
