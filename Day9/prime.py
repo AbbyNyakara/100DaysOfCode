1f8277e558bf54bf544aa7b49e591d4457038a30
Abigael Nyakara <mogusuab@msu.edu>
2023-10-19 09:20:40 -0400

    updates

--- Day9/prime.py
+++ Day9/prime.py
@@ -1,1 +1,14 @@
 # Write a program that takes a number as an input and checks whether its a prime/no
+
+def prime_checker(num):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+    if is_prime:
+        print(f"{num} is prime")
+    else:
+        print(f"{num} is not prime")
+
+
+prime_checker(6)
