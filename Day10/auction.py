9419fb1bb456347ac06883037aec5249084f0e43
Abigael Nyakara <mogusuab@msu.edu>
2023-10-29 21:31:48 -0400

    updates

--- Day10/auction.py
+++ Day10/auction.py
@@ -10,6 +10,16 @@ all_bids = {}
 #Check if the bid is still on 
 bid_finished = False
 
+# Create a function to loop through the dict 
+def highest_bidder(bids):
+    highest_bid = 0
+    winner = ""
+    for name,bid_amount in bids.items():
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = name
+    print(f"The winner is {winner} with an amount of {highest_bid}")
+
 while not bid_finished:
     # Ask the user for the name and the bid amount
     name = input("What is your name? \n")
@@ -20,22 +30,5 @@ while not bid_finished:
     if is_on == 'no':
         #clear screen
         bid_finished = True
-
-print(all_bids)
-# Create a function to loop through the dict 
-
-def highest_bidder(bids):
-    highest_bid = 0
-    winner = ""
-    for name,bid_amount in bids.items():
-        if bid_amount > highest_bid:
-            highest_bid = bid_amount
-            winner = name
-    print(f"The winner is {winner} with an amount of {highest_bid}")
-    
-highest_bidder(all_bids)
-
-
-
-
+        highest_bidder(all_bids)
 
