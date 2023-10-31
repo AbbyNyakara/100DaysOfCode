fb00107e56ebe58c07a35389aae0d437b9f7b891
Abigael Nyakara <mogusuab@msu.edu>
2023-10-31 10:31:39 -0400

    updates

---
+++ Day11/calculator.py
@@ -0,0 +1,31 @@
+
+def add(n1, n2):
+    """
+    Takes two numbers as inputs and returns the sum
+    """
+    return(n1+n2)
+
+def multiply(n1, n2):
+    """
+    Takes two numbers as inputs and returns the product
+    """
+    return(n1*n2)
+
+def subtract(n1, n2):
+    """
+    Takes two numbers and subracts the numbers
+    """
+    return (n1 - n2)
+
+def divide(n1, n2):
+    """
+    Takes two numbers and returns the division of the two numbers
+    """
+    return (n1/n2)
+
+math_operations = {
+    add: '+',
+    subtract: '-',
+    divide: '/',
+    multiply: '*'
+}
