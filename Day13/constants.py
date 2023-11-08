e69b64f08851def894c3d4f8e7ce462c19d30eac
Abigael Nyakara <mogusuab@msu.edu>
2023-11-08 18:28:50 -0500

    updates

---
+++ Day13/constants.py
@@ -0,0 +1,8 @@
+# Define the global constants: 
+# These are basically constants that you will never change. 
+
+# Look at the examples below: 
+
+PI = 3.14256
+URL = 'www.google.com'
+TWITTER_HANDLE = "@abby_nyakara"
